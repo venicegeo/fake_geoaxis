59887e90a26fad530e254add7abe95bec875f618
David Bazile <david.bazile@digitalglobe.com>
2017-04-01 14:58:24 -0400

    Add ability to simulate error conditions

--- fake_geoaxis.py
+++ fake_geoaxis.py
@@ -46,15 +46,27 @@ def authorize_form():
     state = flask.request.args.get('state', '')
     redirect_uri = flask.request.args.get('redirect_uri')
     return """
-    <body style="background-color: #36c; margin: 5em; font-family: Verdana;">
-    <form action="{redirect_uri}" style="position: absolute; top: 5em; left: 5em; right: 5em; bottom: 5em; padding: 4em; background-color: white;">
+    <body style="background-color: #36c; font: 12px Verdana;">
+    <form action="{redirect_uri}" style="position: absolute; top: 10vh; left: 10vh; right: 10vh; bottom: 10vh; padding: 4em; background-color: white; box-shadow: 3px 3px 15px 5px rgba(0,0,0,.1);">
         <h1 style="font: 100 50px Verdana">fake-geoaxis</h1>
         <p>state: <input name="state" value="{state}"/></p>
         <p>redirect_uri: <code>{redirect_uri}</code></p>
-        <p>code: <input name="code" value="pretendthisisavalidcode" style="font-family: monospace; width: 400px;"/></p>
+        <p>
+            Simulate Outcome:
+            <div style="margin-left: 2em;">
+                <label><input name="code" checked type="radio" value="{noise}.success.{noise}"/>Success</label><br/>
+                <label><input name="code" type="radio" value="{noise}.raise_bad_code.{noise}"/>Auth Code Already Consumed</label><br/>
+                <label><input name="code" type="radio" value="{noise}.raise_bad_credentials.{noise}"/>Client Credentials Rejected</label><br/>
+                <label><input name="code" type="radio" value="{noise}.raise_bad_redirect_uri.{noise}"/>Redirect URI Rejected</label><br/>
+                <label><input name="code" type="radio" value="{noise}.raise_500.{noise}"/>HTTP 500</label><br/>
+                <label><input name="code" type="radio" value="{noise}.raise_501.{noise}"/>HTTP 501</label><br/>
+                <label><input name="code" type="radio" value="{noise}.raise_502.{noise}"/>HTTP 502</label><br/>
+                <label><input name="code" type="radio" value="{noise}.raise_503.{noise}"/>HTTP 503</label><br/>
+            </div>
+        </p>
         <p><button>Authorize login, gogogo!</button></p>
     </form>
-    """.format(state=state, redirect_uri=redirect_uri)
+    """.format(noise='0' * 30, state=state, redirect_uri=redirect_uri)
 
 
 @app.route('/ms_oauth/oauth2/endpoints/oauthservice/tokens', methods=['POST'])
@@ -65,6 +77,19 @@ def issue_token():
     print('BODY:')
     pprint(flask.request.get_data(as_text=True))
     print('-' * 80)
+
+    code = flask.request.form.get('code', '').strip('0').strip('.')  # type: str
+    if code.startswith('raise_'):
+        code = code[6:]
+        print('Simulating callback failure')
+        if code == 'bad_code':
+            return flask.jsonify(error='invalid_grant', error_description='Invalid Grant: grant has been revoked'), 401
+        elif code == 'bad_credentials':
+            return flask.jsonify(error='invalid_client', error_description='Invalid client credentials'), 401
+        elif code == 'bad_redirect_uri':
+            return flask.jsonify(error='invalid_client', error_description='redirect_uri parameter value is invalid'), 401
+        return flask.jsonify(error='oh no'), int(code) if code.isdigit() else 500
+
     return flask.jsonify({
         'expires_in': 3600,
         'token_type': 'Bearer',
@@ -80,6 +105,10 @@ def get_profile():
     print('BODY:')
     pprint(flask.request.get_data(as_text=True))
     print('-' * 80)
+
+    if flask.request.headers.get('Authorization') != 'Bearer ACCESS_TOKEN_TIME!':
+        return flask.jsonify(error='sorry, bad token'), 401
+
     return flask.jsonify({
         "uid": "FAKEGEOAXIS",
         "mail": "FAKEGEOAXIS@localhost",
